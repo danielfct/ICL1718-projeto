65caa2dfd0ff155e6173f07a4a72365841053ab7
João Costa Seco <jrcs@fct.unl.pt>
2017-09-16 22:41:29 +0100

    Materials for lab2 are introduced

--- labs/src/tests/InterpreterTests.java
+++ labs/src/tests/InterpreterTests.java
@@ -17,8 +17,16 @@ public class InterpreterTests {
 	}
 	
 	private void testNegativeCase(String expression, int value) throws ParseException {
-		assertFalse(Console.acceptCompare(expression,value));
+		assertFalse(Console.acceptCompare(expression, value));
 	}
+	
+	// private void testCaseBool(String expression, boolean value) throws ParseException {
+	// 	assertTrue(Console.acceptCompareBool(expression, new BoolValue(value)));		
+	// }
+	
+	// private void testNegativeCaseBool(String expression, boolean value) throws ParseException {
+	// 	assertFalse(Console.acceptCompareBool(expression, new BoolValue(value)));
+	// }
 	
 	@Test
 	public void test01() throws Exception {
@@ -31,5 +39,16 @@ public class InterpreterTests {
 	public void testsLabClass02() throws Exception {
 		testCase("4*2\n",8);
 		testCase("4/2/2\n",1);
+		testCase("-1\n", -1);
+		testCase("-1*3\n",-3);
+		// testCaseBool("true\n",true);
+		// testCaseBool("false\n",false);
+		// testCaseBool("11 < 22\n", true);
+		// testCaseBool("11 > 22\n", false);
+		// testCaseBool("11 == 22\n", false);
+		// testCaseBool("3*5 != 1+2 == true\n", true);
+		// testCaseBool("1 == 2 && 3 == 4\n", false);
+		// testCaseNegativeBool("1 == 2 || 3 == 4 && xpto \n", true);
+		// testCaseNegativeBool("!(1 == 2) && xpto \n", true);
 	}
 }
